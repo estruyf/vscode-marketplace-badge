438e5adff22cfe9e6e86a16d763471475761f07d
Elio Struyf <elio@struyfconsulting.be>
2023-06-27 09:59:36 +0200

    Added footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -144,6 +144,26 @@ export default function Home() {
         </div>
       </div>
 
+      <div className="flex flex-wrap justify-center gap-x-1 sm:gap-x-2 lg:justify-start">
+        <a
+          className="flex-none group relative isolate flex items-center rounded-lg px-2 py-0.5 text-[0.8125rem]/6 font-medium text-white transition-colors hover:text-[#97ca00] gap-x-3"
+          href="https://eliostruyf.com/"
+          title="Created by Elio Struyf">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+          </svg>
+
+          <span className="self-baseline text-white">Elio Struyf</span>
+        </a>
+        <a
+          className="flex-none group relative isolate flex items-center rounded-lg px-2 py-0.5 text-[0.8125rem]/6 font-medium text-white transition-colors hover:text-[#97ca00] gap-x-3"
+          href="https://github.com/estruyf/vscode-marketplace-badge"
+          title="Check out the code on GitHub">
+          <svg viewBox="0 0 16 16" aria-hidden="true" fill="currentColor" className="flex-none w-6 h-6"><path d="M8 .198a8 8 0 0 0-8 8 7.999 7.999 0 0 0 5.47 7.59c.4.076.547-.172.547-.384 0-.19-.007-.694-.01-1.36-2.226.482-2.695-1.074-2.695-1.074-.364-.923-.89-1.17-.89-1.17-.725-.496.056-.486.056-.486.803.056 1.225.824 1.225.824.714 1.224 1.873.87 2.33.666.072-.518.278-.87.507-1.07-1.777-.2-3.644-.888-3.644-3.954 0-.873.31-1.586.823-2.146-.09-.202-.36-1.016.07-2.118 0 0 .67-.214 2.2.82a7.67 7.67 0 0 1 2-.27 7.67 7.67 0 0 1 2 .27c1.52-1.034 2.19-.82 2.19-.82.43 1.102.16 1.916.08 2.118.51.56.82 1.273.82 2.146 0 3.074-1.87 3.75-3.65 3.947.28.24.54.73.54 1.48 0 1.07-.01 1.93-.01 2.19 0 .21.14.46.55.38A7.972 7.972 0 0 0 16 8.199a8 8 0 0 0-8-8Z"></path></svg>
+          <span className="self-baseline text-white">GitHub</span>
+        </a>
+      </div>
+
       <a href="https://visitorbadge.io/status?path=https%3A%2F%2Fgithub.com%2Festruyf%2Fvscode-marketplace-badge"><img src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2Festruyf%2Fvscode-marketplace-badge&labelColor=%23d9e3f0&countColor=%23dce775&style=flat" /></a>
     </main>
   )
